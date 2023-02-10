3368bb450ff76732bd93ebda23a14191c7b0f673
김지용 <wldyddkssud@naver.com>
2023-02-10 15:59:19 +0900

    feat: app.tsx에 배포사이트에서 콘솔제거 함수 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 // Create React App은 버전 4에서 기본적으로 새로운 JSX 변환을 지원하지만
 // 사용자 지정 설정을 사용하는 경우 jsx없이 작성할 때 TypeScript 오류를 제거하려면
 // 다음이 필요합니다 import React from 'react'.
-import React, { useCallback } from "react";
+import React from "react";
 import Router from "./Shared/Router";
-import { useState } from "react";
 
 function App() {
+  if (process.env.NODE_ENV === "production") {
+    console.log = function no_console() {};
+    console.warn = function no_console() {};
+  }
   return <Router />;
 }
 
